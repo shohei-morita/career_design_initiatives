e85d8fcd8ec6cb3529dcdee1c88c61fe366fa641
shohei morita <shomorita1985@gmail.com>
2020-12-12 16:01:19 +0900

    TOPページレイアウト微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    student_attribute = %i[
+      family_name
+      given_name
+      family_name_kana
+      given_name_kana
+      nickname
+      tel
+      gender
+      birth_year
+      birth_month
+      birth_day
+    ]
+    devise_parameter_sanitizer.permit(:sign_up, keys: student_attribute)
+    devise_parameter_sanitizer.permit(:update, keys: student_attribute)
+  end
 end
