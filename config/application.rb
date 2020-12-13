52e1ea8330220caca5830b7ede71237d30a1ae68
shohei morita <shomorita1985@gmail.com>
2020-12-13 10:22:14 +0900

    deviseの日本語化

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ module CareerDesignInitiatives
   class Application < Rails::Application
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.assets false
       g.test_framework :rspec,
