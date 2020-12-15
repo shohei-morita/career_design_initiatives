eb8a7d240453857d0d0c7e87d50d9e004fbc388b
shohei morita <shomorita1985@gmail.com>
2020-12-16 00:16:33 +0900

    studentsテーブル　生年月日関係のカラム削除および追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_084035) do
+ActiveRecord::Schema.define(version: 2020_12_15_151453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,6 @@ ActiveRecord::Schema.define(version: 2020_12_12_084035) do
     t.string "tel"
     t.boolean "suspended", default: false, null: false
     t.integer "gender"
-    t.date "birth_year"
-    t.date "birth_month"
-    t.date "birth_day"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -35,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_12_084035) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "graduation_year", null: false
+    t.date "birth_date"
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
