bcd8ab87688409ccf62aa7e25c72f1188e7778bb
shohei morita <shomorita1985@gmail.com>
2020-12-30 18:59:39 +0900

    addressモデルテスト実装

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :address do
+    postcode { 9300017 }
+    prefecture_code { 1 }
+    address_city { '富山県' }
+    address_street { '富山市' }
+    address_building { 'どこか' }
+    association :student, factory: :student
   end
 end
