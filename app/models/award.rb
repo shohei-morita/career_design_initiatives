0fc811f6dcd742f655e4a2fa5764ffe4c1dbc199
Shohei Morita <shomorita1985@gmail.com>
2021-01-08 23:07:40 +0900

    award バリデーション設定

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,3 +1,7 @@
 class Award < ApplicationRecord
   belongs_to :student
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :year, presence: true
+  validates :summary, length: { maximum: 500 }
 end
