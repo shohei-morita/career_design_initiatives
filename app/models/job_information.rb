4d2568a9e67b42e7a43c4f9f3547b6e41cba3af8
Shohei Morita <shomorita1985@gmail.com>
2021-01-18 22:30:40 +0900

    job_information カラムの日本語化

--- app/models/job_information.rb
+++ app/models/job_information.rb
@@ -5,11 +5,12 @@ class JobInformation < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 1000 }
   validates :appealing_point, presence: true, length: { maximum: 500 }
-  validates :pay, presence: true, lenght: { maximum: 100 }
+  validates :pay, presence: true, length: { maximum: 100 }
   validates :working_status, presence: true, length: { maximum: 100 }
   validates :working_hour, presence: true, length: { maximum: 100 }
   validates :benefit, presence: true, length: { maximum: 200 }
   validates :day_off, presence: true, length: { maximum: 150 }
-  validates :selectionm, presence: true, length: { maximum: 500 }
+  validates :selection, presence: true, length: { maximum: 500 }
+  validates :status, presence: true
 
 end
