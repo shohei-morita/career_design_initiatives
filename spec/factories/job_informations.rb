e3233f139c0b5a15747af3cdb38621351bab763c
Shohei Morita <shomorita1985@gmail.com>
2021-01-21 21:57:26 +0900

    job_information E2Eテスト実装

--- spec/factories/job_informations.rb
+++ spec/factories/job_informations.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :job_information do
-    title { 'job' }
-    content { 'good_job' }
-    appealing_point { 'white_company' }
-    pay { '4_000yen' }
-    working_status { 'part_time_job' }
-    working_hour { '15hour_per_a_day' }
-    benefit { 'nothing_special' }
-    day_off { '100_days_off_per_a_year' }
-    selection { 'interview' }
+    sequence(:title, 'job_1')
+    sequence(:content, 'good_1')
+    sequence(:appealing_point, 'white_company_n')
+    sequence(:pay, '4_000yen')
+    sequence(:working_status, 'part_time_job_1')
+    sequence(:working_hour, '15hour_per_a_day_1')
+    sequence(:benefit, 'nothing_special_1')
+    sequence(:day_off, '100_days_off_per_a_year_1')
+    sequence(:selection, 'interview_1')
     status { 0 }
 
     association :company
---
+++ spec/system/job_information_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe 'job_information機能', type: :system do
+  describe 'job_information登録機能' do
+    let!(:approved_recruiter) { create(:recruiter, :approved_recruiter) }
+    let!(:company) { create(:company, recruiter: approved_recruiter) }
+
+    before do
+      login_as approved_recruiter
+      visit recruiter_job_informations_path(approved_recruiter.id)
+    end
+
+    context '求人情報の登録・管理画面にアクセスした場合' do
+      it '登録求人一覧が確認できる' do
+        expect(page).to have_content('job_', count: 5)
+        expect(page).to have_content('準備中', count: 6)
+      end
+
+      it '新規求人登録を選択して、求人情報を登録できる' do
+        click_link '新規求人登録'
+
+        fill_in 'job_information[title]', with: 'ブラック企業へようこそ'
+        fill_in 'job_information[content]', with: '追い出し部屋の管理職候補'
+        fill_in 'job_information[appealing_point]', with: '3年後退職率85%'
+        fill_in 'job_information[pay]', with: '100,000円／月の高待遇'
+        fill_in 'job_information[working_status]', with: '正社員'
+        fill_in 'job_information[working_hour]', with: '7:00〜23:00（休憩なし）'
+        fill_in 'job_information[benefit]', with: '特になし'
+        fill_in 'job_information[day_off]', with: '特になし'
+        fill_in 'job_information[selection]', with: '面接なし'
+        select '準備中', from: 'job_information[status]'
+
+        click_button '保存'
+
+        expect(page).to have_content('準備中求人')
+        expect(page).to have_content('準備中')
+        expect(page).to have_content('ブラック企業へようこそ')
+        expect(page).to have_content('追い出し部屋の管理職候補')
+      end
+
+      it '確認を押して、編集ができる' do
+        click_link '確認', match: :first
+
+        fill_in 'job_information[title]', with: 'ブラック企業へようこそ'
+        fill_in 'job_information[content]', with: '追い出し部屋の管理職候補'
+        fill_in 'job_information[appealing_point]', with: '3年後退職率85%'
+        fill_in 'job_information[pay]', with: '100,000円／月の高待遇'
+        fill_in 'job_information[working_status]', with: '正社員'
+        fill_in 'job_information[working_hour]', with: '7:00〜23:00（休憩なし）'
+        fill_in 'job_information[benefit]', with: '特になし'
+        fill_in 'job_information[day_off]', with: '特になし'
+        fill_in 'job_information[selection]', with: '面接なし'
+        select '公開中', from: 'job_information[status]'
+
+        click_button '保存'
+
+        expect(page).to have_content('公開中求人')
+        expect(page).to have_content('ブラック企業へようこそ')
+        expect(page).to have_content('追い出し部屋の管理職候補')
+      end
+
+      it '任意の求人を削除できる' do
+        click_link '求人削除', match: :first
+
+        expect do
+          page.accept_confirm '本当に削除しますか？'
+          expect(page).to have_content '求人情報を削除'
+        end.to change { JobInformation.count }.by(-1)
+      end
+    end
+  end
+end
