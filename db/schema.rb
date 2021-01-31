774fdb8cb0ce1933316ba0e23294f91f3ce23a4f
Shohei Morita <shomorita1985@gmail.com>
2021-01-31 16:44:08 +0900

    conditionモデルおよびテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_025501) do
+ActiveRecord::Schema.define(version: 2021_01_31_073948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,13 @@ ActiveRecord::Schema.define(version: 2021_01_31_025501) do
     t.datetime "updated_at", null: false
     t.text "revenue"
     t.index ["recruiter_id"], name: "index_companies_on_recruiter_id"
+  end
+
+  create_table "conditions", force: :cascade do |t|
+    t.string "type"
+    t.integer "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "educational_backgrounds", force: :cascade do |t|
