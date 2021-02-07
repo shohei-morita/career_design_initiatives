c4824192433216aca0c335786354d4a394fd5aed
Shohei Morita <shomorita1985@gmail.com>
2021-02-07 18:47:41 +0900

    seedデータ取り込みエラー発生につき、recruiterテーブルにcomfirmation関連カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_223724) do
+ActiveRecord::Schema.define(version: 2021_02_07_093314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,11 +140,14 @@ ActiveRecord::Schema.define(version: 2021_02_06_223724) do
   end
 
   create_table "qualifications", force: :cascade do |t|
-    t.string "ancestry"
-    t.string "name", null: false
+    t.string "qualification_1st"
+    t.string "qualification_2nd"
+    t.string "qualification_3rd"
+    t.string "qualification_4th"
+    t.string "qualification_5th"
+    t.text "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["ancestry"], name: "index_qualifications_on_ancestry"
   end
 
   create_table "recruiters", force: :cascade do |t|
@@ -170,6 +173,7 @@ ActiveRecord::Schema.define(version: 2021_02_06_223724) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_recruiters_on_confirmation_token", unique: true
     t.index ["email"], name: "index_recruiters_on_email", unique: true
     t.index ["reset_password_token"], name: "index_recruiters_on_reset_password_token", unique: true
   end
