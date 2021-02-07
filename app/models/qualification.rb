562d1d1194db55a79a96a19f7620884793354882
Shohei Morita <shomorita1985@gmail.com>
2021-02-07 21:59:44 +0900

    qualification modelテスト実装

--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -1,3 +1,9 @@
 class Qualification < ApplicationRecord
   belongs_to :student
+  validates :qualification_1st, length: { maximum: 50 }
+  validates :qualification_2nd, length: { maximum: 50 }
+  validates :qualification_3rd, length: { maximum: 50 }
+  validates :qualification_4th, length: { maximum: 50 }
+  validates :qualification_5th, length: { maximum: 50 }
+  validates :note, length: { maximum: 500 } 
 end
