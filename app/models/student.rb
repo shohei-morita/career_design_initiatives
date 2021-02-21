c2241e35bcaadf966bf26a364a45c5c931f0f147
Shohei Morita <shomorita1985@gmail.com>
2021-02-21 12:54:19 +0900

    企業申請確認画面修正

--- app/models/student.rb
+++ app/models/student.rb
@@ -56,15 +56,15 @@ class Student < ApplicationRecord
   end
 
   def thumbnail
-    avatar.variant(resize: '150x150').processed
+    self.avatar.variant(resize: '150x150').processed
   end
 
   def profile_image
-    avatar.variant(resize: '200x200').processed
+    self.avatar.variant(resize: '200x200').processed
   end
 
   def header_image
-    avatar.variant(resize: '200x200').processed
+    self.avatar.variant(resize: '200x200').processed
   end
 
   def update_without_current_password(params, *options)
