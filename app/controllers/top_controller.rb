fcd10630037bf6ace54ef2e80fcdce98f68fc5ec
Shohei Morita <shomorita1985@gmail.com>
2021-02-24 20:27:00 +0900

    ログインユーザーのアプリアクセス時の遷移先修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,11 +1,11 @@
 class TopController < ApplicationController
   def top
     if current_student
-      redirect_to home_students_path
+      redirect_to students_path
     elsif current_recruiter.try(:admin) == false
-      redirect_to home_recruiters_path
+      redirect_to recruiters_path
     elsif current_recruiter.try(:admin) == true
-      redirect_to admin_recruiters_path
+      redirect_to home_admin_recruiters_path
     else
       render :top
     end
