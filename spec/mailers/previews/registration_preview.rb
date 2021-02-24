03401a3c70db5909d871478984a35fac21292889
Shohei Morita <shomorita1985@gmail.com>
2021-02-25 00:12:56 +0900

    不要テストの削除

--- spec/mailers/previews/registration_preview.rb
+++
@@ -1,7 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/registration
-class RegistrationPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/registration/approval
-  def approval
-    RegistrationMailer.approval
-  end
-end
